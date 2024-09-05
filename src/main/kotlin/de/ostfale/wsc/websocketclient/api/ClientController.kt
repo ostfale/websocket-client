f6fb3f70dac68bceb21f7b427dfff32e0e51b025
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2024-09-05 18:47:03 +0200

    Bootstrap some classes

---
+++ src/main/kotlin/de/ostfale/wsc/websocketclient/api/ClientController.kt
@@ -0,0 +1,8 @@
+package de.ostfale.wsc.websocketclient.api
+
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController()
+class ClientController {
+}
---
+++ src/main/kotlin/de/ostfale/wsc/websocketclient/ws/ClienWSConfiguration.kt
@@ -0,0 +1,7 @@
+package de.ostfale.wsc.websocketclient.ws
+
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class ClienWSConfiguration {
+}
