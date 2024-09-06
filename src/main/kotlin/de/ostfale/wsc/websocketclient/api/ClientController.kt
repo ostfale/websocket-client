e4fcb17d89b2fed4186e9863f3f803b9273fa8c9
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2024-09-06 14:50:24 +0200

    Fix Controller

--- src/main/kotlin/de/ostfale/wsc/websocketclient/api/ClientController.kt
+++ src/main/kotlin/de/ostfale/wsc/websocketclient/api/ClientController.kt
@@ -1,8 +1,11 @@
 package de.ostfale.wsc.websocketclient.api
 
 import de.ostfale.wsc.websocketclient.service.ClientWSTriggerService
+import de.ostfale.wsc.websocketclient.ws.TCWebSocketSession
 import org.slf4j.LoggerFactory
-import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -14,9 +17,22 @@ class ClientController(
 ) {
     private val log = LoggerFactory.getLogger(javaClass)
 
-    @PostMapping("/connect")
+    @GetMapping("/connect")
     fun connect() {
         log.info("CONTROLLER :: Connect to WebSocket service")
         clientWSTriggerService.connect()
+    }
+
+    @GetMapping("/sessionid")
+    fun getClientSession(): ResponseEntity<String?> {
+        log.info("GET Request for clientSession")
+        val clientSessionId = TCWebSocketSession.getSessionId()
+        return ResponseEntity(clientSessionId, HttpStatus.OK)
+    }
+
+    @GetMapping("/ping")
+    fun receivedPing(): ResponseEntity<String> {
+        log.info("GET Request for ping")
+        return ResponseEntity("Pong", HttpStatus.OK)
     }
 }
